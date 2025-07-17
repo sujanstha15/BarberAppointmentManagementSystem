719cbeed6f7cbab1f8387cfb3db53cf1576a2359
Sujan Shrestha <raju40rr@gmail.com>
2025-07-17 13:44:56 -0500

    First Commit

---
+++ barbermanagement/barbermanagement/src/main/java/com/barberappointmentmanagementsystem/BarbermanagementApplication.java
@@ -0,0 +1,13 @@
+package com.barberappointmentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbermanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbermanagementApplication.class, args);
+	}
+
+}
